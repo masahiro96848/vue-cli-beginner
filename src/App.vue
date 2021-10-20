694ac4ff84fd74f6e36ea5cc92faf4127f74ff3f
masahiro96848 <feetwcpic9436@gmail.com>
2021-10-20 22:57:40 +0900

    データの双方向v-modelを追加

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,23 @@
 	<section id="app" class="todoapp">
 		<header class="header">
 			<h1>todos</h1>
+            <input 
+                class="new-todo"
+                autofocus autocomplete="off"
+                type="text" 
+                placeholder="What needs to be done"
+                v-model="todo.title"
+
+            >
 		</header>
 		<section class="main">
 			<ul class="todo-list">
 				<li class="todo">
 					<div class="view">
+                        <input 
+                            type="checkbox"
+                            class="toggle"
+                        >
 						<label>{{todo.title}}</label>
 					</div>
 				</li>
