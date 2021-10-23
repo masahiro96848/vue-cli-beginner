448fc1e08f636ca18ed5daf88a80a85d6c61c7ac
masahiro96848 <feetwcpic9436@gmail.com>
2021-10-23 11:15:03 +0900

    リスト表示する項目を選び出して切り替える

---
+++ src/components/TodoController.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="footer" v-show="todos.length" v-cloak>
+        <span class="todo-count">
+            <strong>{{ remaining}}</strong> {{ remaining | pluralize}} left
+        </span>
+        <ul class="filters">
+            <li><a href="#/all" :class="{selected: visibility === 'all'}">全て</a></li>
+            <li><a href="#/active" :class="{selected: visibility === 'active'}">未処理</a></li>
+            <li><a href="#/completed" :class="{selected: visibility === 'completed'}">完了</a></li>
+        </ul>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'TodoController',
+    filters: {
+		pluralize(n) {
+			return n === 1 ? 'item' : 'items';
+		}
+	},
+	props: {
+		todos: Array,
+		remaining: Number,
+		visibility: String
+	}
+}
+</script>
